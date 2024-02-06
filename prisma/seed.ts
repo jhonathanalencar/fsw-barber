19106ae2bc1e3fcdc757fa6388d6c0f1cb06ee2f
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2024-02-06 19:50:35 -0300

    fix: seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,6 +4,9 @@ const prisma = new PrismaClient();
 
 async function seedDatabase() {
   try {
+    await prisma.service.deleteMany();
+    await prisma.barbershop.deleteMany();
+
     const images = [
       "https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png",
       "https://utfs.io/f/45331760-899c-4b4b-910e-e00babb6ed81-16q.png",
@@ -95,7 +98,8 @@ async function seedDatabase() {
         name: "Hidratação",
         description: "Hidratação profunda para cabelo e barba.",
         price: 25.0,
-        imageUrl: "Fios hidratados, macios e brilhantes.",
+        imageUrl:
+          "https://utfs.io/f/c4919193-a675-4c47-9f21-ebd86d1c8e6a-4oen2a.png",
       },
     ];
 
