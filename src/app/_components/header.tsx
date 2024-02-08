24bfabb04f156a958dde53ddc89988a2ae0e363d
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2024-02-07 23:12:46 -0300

    feat: use semantic tags on header and footer

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -8,23 +8,25 @@ import { SideMenu } from "./side-menu";
 
 export function Header() {
   return (
-    <Card>
-      <CardContent className="p-5 justify-between items-center flex flex-row">
-        <Link href="/">
-          <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
-        </Link>
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button variant="outline" size="icon">
-              <MenuIcon size={16} />
-            </Button>
-          </SheetTrigger>
+    <header>
+      <Card>
+        <CardContent className="p-5 justify-between items-center flex flex-row">
+          <Link href="/">
+            <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
+          </Link>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon">
+                <MenuIcon size={16} />
+              </Button>
+            </SheetTrigger>
 
-          <SheetContent className="p-0">
-            <SideMenu />
-          </SheetContent>
-        </Sheet>
-      </CardContent>
-    </Card>
+            <SheetContent className="p-0">
+              <SideMenu />
+            </SheetContent>
+          </Sheet>
+        </CardContent>
+      </Card>
+    </header>
   );
 }
